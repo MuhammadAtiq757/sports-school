e7d34a4066124ba839a61686d03c1d96b177208c
MuhammadAtiq <muhammadatiq757@gamil.com>
2023-06-14 21:46:25 +0600

    text added

--- src/Pages/Home/Home/Banner/Banner.jsx
+++ src/Pages/Home/Home/Banner/Banner.jsx
@@ -5,10 +5,10 @@ import slide4 from '../../../../../public/images/slide-4.jpg';
 
 const Banner = () => {
   const slideTitles = [
-    "Football",
-    "Basketball",
-    "Tennis",
-    "Swimming"
+    "Yoga is a holistic practice that combines physical postures, breathing exercises.",
+    "Yoga is a holistic practice that combines physical postures, breathing exercises.",
+    "Yoga is a holistic practice that combines physical postures, breathing exercises.",
+    "Yoga is a holistic practice that combines physical postures, breathing exercises."
   ];
 
   return (
@@ -16,7 +16,7 @@ const Banner = () => {
       <div className="carousel w-full h-96">
         <div id="slide1" className="carousel-item relative w-full">
           <img className="w-full" src={slide1} alt="Slide 1" />
-          <h3 className="absolute top-2 left-2 text-white font-semibold">{slideTitles[0]}</h3>
+          <h3 className="absolute text-yellow-100 mt-36  mx-8 top-2 left-2  text-3xl w-[40%] ml-12 font-extrabold text-black">{slideTitles[0]}</h3>
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a href="#slide4" className="btn btn-circle">
               ❮
@@ -28,7 +28,7 @@ const Banner = () => {
         </div>
         <div id="slide2" className="carousel-item relative w-full">
           <img src={slide2} className="w-full" alt="Slide 2" />
-          <h3 className="absolute top-2 left-2 text-white font-semibold">{slideTitles[1]}</h3>
+          <h3 className="absolute mt-36 text-yellow-100  mx-8 top-2 left-2  text-3xl w-[40%] ml-12 font-extrabold text-black">{slideTitles[1]}</h3>
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a href="#slide1" className="btn btn-circle">
               ❮
@@ -40,7 +40,7 @@ const Banner = () => {
         </div>
         <div id="slide3" className="carousel-item relative w-full">
           <img src={slide3} className="w-full" alt="Slide 3" />
-          <h3 className="absolute top-2 left-2 text-white font-semibold">{slideTitles[2]}</h3>
+          <h3 className="absolute text-yellow-100 mt-36  mx-8 top-2 left-2  text-3xl w-[40%] ml-12 font-extrabold text-black">{slideTitles[2]}</h3>
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a href="#slide2" className="btn btn-circle">
               ❮
@@ -52,7 +52,7 @@ const Banner = () => {
         </div>
         <div id="slide4" className="carousel-item relative w-full">
           <img src={slide4} className="w-full" alt="Slide 4" />
-          <h3 className="absolute top-2 left-2 text-white font-semibold">{slideTitles[3]}</h3>
+          <h3 className="absolute text-yellow-100 mt-36  mx-8 top-2 left-2  text-3xl w-[40%] ml-12 font-extrabold text-black">{slideTitles[3]}</h3>
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a href="#slide3" className="btn btn-circle">
               ❮
